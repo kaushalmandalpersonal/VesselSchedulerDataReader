3ce1b819c5a5a5cc3041dd63108a986bdc2c73e5
Kaushal Mandal <kaushal.mandal@ibm.com>
2022-07-27 17:39:25 +0530

    First commit.

---
+++ src/main/java/com/example/VesselSchedulerDataReader/VesselSchedulerDataReaderApplication.java
@@ -0,0 +1,13 @@
+package com.example.VesselSchedulerDataReader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VesselSchedulerDataReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VesselSchedulerDataReaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/VesselSchedulerDataReader/VesselSchedulerDataReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.VesselSchedulerDataReader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VesselSchedulerDataReaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
